3fc9c63f1097c839cc009e26e45949af47095a57
Bastian Bresson <bastianlol@live.dk>
2020-05-02 20:34:04 +0200

    Added editor window. Added delete save file to that window

---
+++ Assets/Scripts/EditorScripts/EditModeFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class EditModeFunctions : EditorWindow
+{
+    private static string dataSubPath = "/AWizardsMemorySave.json";
+    private static string dataPath;
+
+    [MenuItem("Window/Edit Mode Functions")]
+    public static void ShowWindow()
+    {
+        GetWindow<EditModeFunctions>("Edit Mode Functions");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Delete Save File"))
+        {
+            DeleteSaveFile();
+        }
+    }
+
+    private void DeleteSaveFile()
+    {
+        File.Delete(dataPath);
+        if (File.Exists(dataPath))
+        {
+            Debug.Log("Unsucsesful: save file still exists");
+        }
+        else
+        {
+            Debug.Log("Succesful: save file no longer exists");
+        }
+    }
+
+    private void OnEnable()
+    {
+        dataPath = Application.persistentDataPath + dataSubPath;
+    }
+}
